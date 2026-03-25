64beaa0b63f718df9abde364a8b5289158acf6e9
echiquito-2025144 <echiquito-2025144@kinal.edu.gt>
2026-03-25 17:07:06 -0600

    refactor: se editó IVentaService

--- src/main/java/com/erickchiquito/kinalapp/service/IVentaService.java
+++ src/main/java/com/erickchiquito/kinalapp/service/IVentaService.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IVentaService {
-    List<Venta> findaAll();
-    Optional<Venta> findaById(int id);
+    List<Venta> findAll();
+    Optional<Venta> findById(int id);
     Venta save(Venta venta);
     void deleteById(int id);
 }
