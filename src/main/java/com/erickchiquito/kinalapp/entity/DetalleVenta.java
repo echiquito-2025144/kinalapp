344cb429ea06df609c6e64c79d76256d5a27f3da
Sebastian1860 <echiquito-2025144@kinal.edu.gt.com>
2026-03-25 21:42:53 -0600

    feat: se creó DetalleVenta Entity

---
+++ src/main/java/com/erickchiquito/kinalapp/entity/DetalleVenta.java
@@ -0,0 +1,79 @@
+package com.erickchiquito.kinalapp.entity;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "DetalleVenta")
+public class DetalleVenta {
+
+    @Id
+    @Column(name = "codigo_detalle_venta")
+    private int codigoDetalleVenta;
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    @Column(name = "precio_unitario")
+    private BigDecimal precioUnitario;
+
+    @Column(name = "subtotal")
+    private BigDecimal subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "ventas_codigo_venta")
+    private Venta venta;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(int codigoDetalleVenta, int cantidad, BigDecimal precioUnitario, BigDecimal subtotal, Venta venta) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+        this.venta = venta;
+    }
+
+    public int getCodigoDetalleVenta() {
+        return codigoDetalleVenta;
+    }
+
+    public void setCodigoDetalleVenta(int codigoDetalleVenta) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+}
