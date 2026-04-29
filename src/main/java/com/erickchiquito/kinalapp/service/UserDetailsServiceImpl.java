4a1b088b045d54e0613a2804f2b1949fab1b40f1
echiquito-2025144 <informatica@alumno.local>
2026-04-29 16:23:11 -0600

    feat: se creó UserDetailService

---
+++ src/main/java/com/erickchiquito/kinalapp/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.erickchiquito.kinalapp.service;
+
+import com.erickchiquito.kinalapp.entity.Usuario;
+import com.erickchiquito.kinalapp.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+
+        return User.withUsername(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles(usuario.getRol()) // Tu campo "rol" de la DB
+                .build();
+    }
+}
